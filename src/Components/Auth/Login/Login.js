c0f393c931fe16634776491f5db1ba73bb6ac1a7
NazmulHasanNahid <nh.nahid4491@gmail.com>
2022-04-17 07:28:17 +0600

    create-user

--- src/Components/Auth/Login/Login.js
+++ src/Components/Auth/Login/Login.js
@@ -1,8 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Login.css";
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from "../../../firebase.init";
+
 
 const Login = () => {
+     const [
+          signInWithEmailAndPassword,
+          user,
+          loading,
+          error,
+        ] = useSignInWithEmailAndPassword(auth);
+        const [userInfo , setUserInfo] = useState({
+          name: "" ,
+          email: "" ,
+          password:"" ,
+     })
+     const [errors ,  setErrors] = useState({
+          email:"" ,
+          password:"",
+     })
+        const handleEmail = (e) =>{
+          const emailRegex = /\S+@\S+\.\S+/
+          const validEmail =   emailRegex.test(e.target.value)
+          if(validEmail){
+      
+            setUserInfo({...userInfo , email: e.target.value})
+            setErrors({...errors , email:''})
+          }else{
+            setErrors({...errors , email:'Invalid Email'})
+          }
+     }
+
+     const handlePassword = (e) =>{
+          const passwordRegex = /.{6}/;
+          const validPassword = passwordRegex.test(e.target.value)
+          if(validPassword){
+        
+           setUserInfo({...userInfo , password:e.target.value})
+           setErrors({...errors , password:''})
+          }else{
+            setErrors({...errors , password:'Password Must Be  contain 6 characters'})
+          }
+        }
+        const handleCreateAccount = (e) =>{
+          e.preventDefault()
+        }
   return (
     <div>
       <form action="">
--- src/Components/Auth/SignUp/SignUp.js
+++ src/Components/Auth/SignUp/SignUp.js
@@ -1,10 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from "../../../firebase.init";
+
+
+
+
 
 const SignUp = () => {
+     const [
+          createUserWithEmailAndPassword,
+          user,
+          loading,
+          error,
+        ] = useCreateUserWithEmailAndPassword(auth);
+
+     const [userInfo , setUserInfo] = useState({
+          name: "" ,
+          email: "" ,
+          password:"" ,
+     })
+     const [errors ,  setErrors] = useState({
+          email:"" ,
+          password:"",
+     })
+     const handleEmail = (e) =>{
+          const emailRegex = /\S+@\S+\.\S+/
+          const validEmail =   emailRegex.test(e.target.value)
+          if(validEmail){
+      
+            setUserInfo({...userInfo , email: e.target.value})
+            setErrors({...errors , email:''})
+          }else{
+            setErrors({...errors , email:'Invalid Email'})
+          }
+     }
+
+     const handlePassword = (e) =>{
+          const passwordRegex = /.{6}/;
+          const validPassword = passwordRegex.test(e.target.value)
+          if(validPassword){
+        
+           setUserInfo({...userInfo , password:e.target.value})
+           setErrors({...errors , password:''})
+          }else{
+            setErrors({...errors , password:'Password Must Be  contain 6 characters'})
+          }
+        }
+        const handleCreateAccount = (e) =>{
+          e.preventDefault()
+          createUserWithEmailAndPassword(userInfo.email, userInfo.password)
+        }
   return (
     <div>
-      <form>
+      <form onSubmit={handleCreateAccount}>
         <div className="login">
           <h1 className="title">Create Account</h1>
           <div className="container">
@@ -14,16 +63,19 @@ const SignUp = () => {
                 <label htmlFor="email">Name</label>
               </div>
               <div className="form-field col-lg-6">
-                <input className="input-text" type="email" name="email" />
+                <input onChange={handleEmail} className="input-text" type="email" name="email" />
                 <label htmlFor="email">Email</label>
+                {errors?.email && <p className="text-danger my-3 fw-bold">{errors?.email}</p>}
               </div>
               <div className="form-field col-lg-6">
-                <input className="input-text" type="password" name="password" />
+                <input onChange={handlePassword} className="input-text" type="password" name="password" />
                 <label htmlFor="password">Password</label>
               </div>
               <div className="form-field col-lg-6">
                 <input className="input-text" type="password" name="password" />
                 <label htmlFor="password">Confirm Password</label>
+                {errors?.password && <p className="text-danger my-3 fw-bold">{errors?.password}</p>}
+
               </div>
               <div className="form-field col-lg-12">
                 <input
